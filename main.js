0a6d1fb97b79573ad763a53088248b4d171635a1
Caleb Rowland <cjrowland4@gmail.com>
2024-06-19 08:30:40 -0500

    Finish 'project'. Think this is the last intro project

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const input = require('sync-input');
 
+
 let name = input();
 let surname = input();
 let message = input();
+let numberOfRepeats = Number(input());
 
-let i = 0;
-while (i < 5) {
+for (let i = 0; i < numberOfRepeats; i++) {
   console.log(`This is ${name} ${surname} and ${message}.`);
-  i++;
 }
